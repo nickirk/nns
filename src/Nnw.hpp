64ca491f8e5572706911662566008db523307cb9
Ke Liao <ke.liao.whu@gmail.com>
2017-10-23 21:06:47 +0200

    finished complex coefficients and momentum in optimising coefficients

--- src/Nnw.hpp
+++ src/Nnw.hpp
@@ -11,37 +11,46 @@
 #include <vector>
 #include <stdio.h>
 #include <math.h>
+#include <complex>
 #include <Eigen/Dense>
 #include "Determinant.hpp"
 #include "Basis.hpp"
 #include "Hamiltonian.hpp"
 //#include "Sampler.hpp"
+const std::complex<double> ii(0.,1.);
 class NeuralNetwork{
   public:
     NeuralNetwork(std::vector<int> const &sizes_, Hamiltonian const&H_,
       Basis const &fullBasis_);
     std::vector<detType> train(std::vector<detType> const&listDetsToTrain, double eta);
     double getEnergy(){return energy;}
-    double getSampleEnergy(){return sampleEnergy;}
-    std::vector<double> getEnergyDerivative(std::vector<detType> const &list){return NablaE_C(list);}
+    std::vector<Eigen::VectorXd> getEnergyDerivative(std::vector<detType> const &list){return NablaE_C(list);}
     int getSign(){return sign;}
-    std::vector<double> getCs() const {return output_Cs;}
+    std::vector<Eigen::VectorXd> getCs() const {return output_Cs;}
     Eigen::VectorXd feedForward(detType const& det);
   private:
     int sign;
+    double momentumDamping;
+    bool momentum;
     std::vector<int> sizes;
     std::vector<Eigen::VectorXd> biases;
     std::vector<Eigen::MatrixXd> weights;
-    std::vector<double> output_Cs;
+    std::vector<Eigen::VectorXd> output_Cs;
     std::vector<Eigen::VectorXd> inputSignal; 
     std::vector<Eigen::VectorXd> activations; 
     std::vector<Eigen::VectorXd> nabla_biases;
     std::vector<Eigen::MatrixXd> nabla_weights;
+    std::vector<Eigen::VectorXd> nabla_biasesPrev;
+    std::vector<Eigen::MatrixXd> nabla_weightsPrev;
+    std::vector<Eigen::VectorXd> g_biases;
+    std::vector<Eigen::MatrixXd> g_weights;
+    std::vector<Eigen::VectorXd> g_biasesPrev;
+    std::vector<Eigen::MatrixXd> g_weightsPrev;
     //std::vector<detType> &listDetsToTrain;
     Hamiltonian const&H;
     Basis const&fullBasis;
     double energy;
-    double sampleEnergy;
+    double normalizerCoeff;
     void calcEnergy(std::vector<detType> const &listDetsToTrain);
     //void calcLocalEnergy(std::vector<detType> const &listDetsToTrain);
     //std::vector<detType> train(std::vector<detType> &listDetsToTrain_, double eta);
@@ -51,11 +60,15 @@ class NeuralNetwork{
            std::vector<std::vector<Eigen::VectorXd>> const &inputSignal_Epochs,
            std::vector<std::vector<Eigen::VectorXd>> const &activations
          );
-    std::vector<double> NablaE_C(std::vector<detType> const &listDetsToTrain);
+    std::vector<Eigen::VectorXd> NablaE_C(std::vector<detType> const &listDetsToTrain);
 };
 
 double Tanh_prime(double in);
 double Tanh(double in); 
 double Linear(double in);
 double Linear_prime(double in);
+double Gaussian(double in);
+double Gaussian_prime(double in);
+double Sigmoid(double in);
+double Sigmoid_prime(double in);
 #endif
--- test/testEigen.cxx
+++ test/testEigen.cxx
@@ -5,23 +5,9 @@
 using namespace Eigen;
 using namespace std;
 int main(){
-MatrixXd ones = MatrixXd::Ones(3,3);
-MatrixXd ones1 = MatrixXd::Ones(3,3);
-EigenSolver<MatrixXd> es(ones);
-EigenSolver<MatrixXd> es1(ones1);
-cout << "The first eigenvector of the 3x3 matrix of ones is:"
-     << endl << es1.eigenvectors().col(0) << endl;
-cout << "type of es1= " << typeid(es1.eigenvectors().col(0)).name() << endl;
-auto eVector1=es.eigenvectors().col(0);
-VectorXcd eVector=es.eigenvectors().col(0);
-//VectorXcd eVector=eVector1;
-cout << "type of eVector= " << typeid(eVector).name() << endl;
-cout << "type of eVector1= " << typeid(eVector1).name() << endl;
-auto eigenValue=es.eigenvalues();
+VectorXd m = VectorXd::Random(3);
 
-cout << "eVector= " << eVector << endl;
-cout << "eVector1= " << eVector1 << endl;
-cout << "eigenValues= " << eigenValue << endl;
+cout << "m[]=" << m(1) << endl;
 }
 
 
