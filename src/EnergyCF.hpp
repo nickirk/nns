e94ce3da7357b035c774ccd1986eca4a58eb9d20
Ke Liao <ke.liao.whu@gmail.com>
2017-11-08 12:18:01 +0100

    Add new cost function and put removeDupliate outside of Sampler class

--- src/EnergyCF.hpp
+++ src/EnergyCF.hpp
@@ -18,8 +18,7 @@ class EnergyCF: public CostFunction{
 public:
 	explicit EnergyCF(Hamiltonian const &H_):CostFunction(),H(H_),energy(0.0),normalizerCoeff(0.0){};
 	std::vector<Eigen::VectorXd> nabla(State const &input) const;
-	double calc(State const &input) const {return energy;};
-        double getNormalizer(){return normalizerCoeff;};
+	double calc(State const &input) const {return energy;}
 private:
 	Hamiltonian const& H;
 	mutable double energy;
