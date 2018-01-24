86bf6fe6ba469324d441a5976bffdfdccb871f31
Kai Guther <k.guther@fkf.mpg.de>
2018-01-24 15:16:43 +0100

    Norm

--- src/MarkovSampler.cxx
+++ src/MarkovSampler.cxx
@@ -30,7 +30,7 @@ void MarkovSampler::iterate(coeffType &cI, detType &dI) const{
 		detType tmp{getRandomCoupledState(cDet,p)};
 		// And its coefficient
 		coeffType tmpCoeff{NNW.getCoeff(tmp)};
-		if(uni(rng) < std::fabs(tmpCoeff/lastCoeff)){
+		if(uni(rng) < std::pow(std::fabs(tmpCoeff/lastCoeff),2)){
 			// With probability cJ/cI, accept the move
 			cDet = tmp;
 			lastCoeff = tmpCoeff;
