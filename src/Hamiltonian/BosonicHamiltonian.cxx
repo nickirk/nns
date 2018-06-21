8b50ac5cf70989e163da13ff6cdf0262dd204eb9
Kai Guther <guther@nbal03.suse>
2018-06-21 10:27:01 +0200

    unrelated changes to bosonic h

--- src/Hamiltonian/BosonicHamiltonian.cxx
+++ src/Hamiltonian/BosonicHamiltonian.cxx
@@ -13,5 +13,10 @@ BosonicHamiltonian::~BosonicHamiltonian() {
 	// TODO Auto-generated destructor stub
 }
 
+std::vector<detType> BosonicHamiltonian::getCoupledStates(
+		detType const &source) const{
+	return std::vector<detType>(0);
+}
+
 }
 
