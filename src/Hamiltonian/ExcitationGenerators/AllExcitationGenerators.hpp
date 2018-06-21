46f5e4da48f798d5faab5b689e5aa86572747365
Kai Guther <guther@nbal03.suse>
2018-06-21 08:28:02 +0200

    utilities for setting the excitgen

---
+++ src/Hamiltonian/ExcitationGenerators/AllExcitationGenerators.hpp
@@ -0,0 +1,21 @@
+/*
+ * AllExcitationGenerators.hpp
+ *
+ *  Created on: Jun 20, 2018
+ *      Author: guther
+ */
+
+#ifndef SRC_HAMILTONIAN_EXCITATIONGENERATORS_ALLEXCITATIONGENERATORS_HPP_
+#define SRC_HAMILTONIAN_EXCITATIONGENERATORS_ALLEXCITATIONGENERATORS_HPP_
+
+#include "ExcitationGenerator.hpp"
+#include "ExcitmatType.hpp"
+#include "ProbUpdater.hpp"
+#include "RSHubbardExcitgen.hpp"
+#include "UniformExcitgen.hpp"
+#include "WeightedExcitgen.hpp"
+#include "WeightedSelector.hpp"
+
+
+
+#endif /* SRC_HAMILTONIAN_EXCITATIONGENERATORS_ALLEXCITATIONGENERATORS_HPP_ */
